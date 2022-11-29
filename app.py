0ddb167677fb30fbcb26120200e6c50fcbddb067
AzharMoosa <azhar.moosa8@gmail.com>
2022-11-29 15:27:37 +0000

    Serve React Folder

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api
+from flask_cors import CORS, cross_origin
 from apispec import APISpec
 from apispec.ext.marshmallow import MarshmallowPlugin
 from flask_apispec.extension import FlaskApiSpec
 from backend.resources.chatbot_api import ChatbotAPI
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='frontend/build/static',
+            template_folder='frontend/build')
+CORS(app)
 api = Api(app)
 
 # Setup Swagger UI
@@ -20,6 +23,14 @@ app.config.update({
     "APISPEC_SWAGGER_UI_URL": "/swagger-ui/"
 })
 docs = FlaskApiSpec(app)
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+@cross_origin()
+def serve(path):
+    return render_template("index.html")
+
 
 api.add_resource(ChatbotAPI, "/chatbot")
 docs.register(ChatbotAPI)
