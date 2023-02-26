60ee054f48b1cd23289e324f7e2fc0e7e058aa6d
AzharMoosa <azhar.moosa8@gmail.com>
2023-02-26 16:27:22 +0000

    Added Skeleton Files For Question Engine

---
+++ backend/common/question_engine/engines/fill_in_blank_engine.py
@@ -0,0 +1,3 @@
+class FillInBlankEngine:
+    def __init__(self, text: str) -> None:
+        self.text = text
---
+++ backend/common/question_engine/engines/true_false_engine.py
@@ -0,0 +1,3 @@
+class TrueOrFalseEngine:
+    def __init__(self, statement) -> None:
+        self.statement = statement
