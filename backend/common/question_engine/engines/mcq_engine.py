3fbb13982df010cacd1f49cde30c6ab8367f0ebc
AzharMoosa <azhar.moosa8@gmail.com>
2023-02-28 11:51:30 +0000

    Implemented Sense2Vec MCQ Question Generator

--- backend/common/question_engine/engines/mcq_engine.py
+++ backend/common/question_engine/engines/mcq_engine.py
@@ -5,6 +5,11 @@ import re
 import random
 import json
 import requests
+from collections import OrderedDict
+import string
+from sense2vec import Sense2Vec
+from rapidfuzz.distance import Levenshtein
+
 nltk.download("wordnet")
 
 
@@ -12,6 +17,7 @@ class MCQEngine:
     def __init__(self, question: str, answer: str) -> None:
         self.question = question
         self.answer = answer
+        self.s2v = Sense2Vec().from_disk("s2v")
 
     def __get_synset(self, word: str):
         return wordnet.synsets(word, "n")
@@ -67,8 +73,46 @@ class MCQEngine:
 
         return distractors
 
+    def __process_s2v_word(self, word: str) -> str:
+        return word[0].split("|")[0].replace("_", " ").lower()
+
+    def __filter_distractors(self, distractors: List[str]) -> List[str]:
+        def get_edits(word):
+            letters = "abcdefghijklmnopqrstuvwxyz " + string.punctuation
+            splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+            deletes = [L + R[1:] for L, R in splits if R]
+            transposes = [L + R[1] + R[0] + R[2:]
+                          for L, R in splits if len(R) > 1]
+            replaces = [L + c + R[1:] for L, R in splits if R for c in letters]
+            inserts = [L + c + R for L, R in splits for c in letters]
+
+            return set(deletes + transposes + replaces + inserts)
+
+        edits = get_edits(self.answer.lower())
+        threshold = 0.7
+
+        filtered_distractors = [
+            word for word in distractors if word.lower() not in edits]
+
+        return [word for word in filtered_distractors if Levenshtein.normalized_distance(word.lower(), self.answer.lower()) > threshold]
+
+    def generate_distractors_sense2vec(self, filter_output: bool = True) -> List[str]:
+        answer = self.answer.lower()
+        answer = answer.replace(" ", "_")
+
+        best_sense = self.s2v.get_best_sense(answer)
+        most_similar = self.s2v.most_similar(best_sense, n=20)
+
+        distractors = [self.__process_s2v_word(
+            word).title() for word in most_similar if word != self.__process_s2v_word(word)]
+
+        distractors = list(OrderedDict.fromkeys(distractors))
+
+        return self.__filter_distractors(distractors) if filter_output else distractors
+
 
 if __name__ == "__main__":
-    m = MCQEngine("ww", "California")
+    m = MCQEngine("ww", "USA")
     # print(m.generate_distractors_wordnet())
-    print(m.generate_distractors_conceptnet())
+    # print(m.generate_distractors_conceptnet())
+    print(m.generate_distractors_sense2vec())
