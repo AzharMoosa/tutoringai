586473beb5da741bca03ac90835f7192b94cb0be
AzharMoosa <azhar.moosa8@gmail.com>
2023-05-10 11:54:12 +0100

    Refactoring

--- frontend/src/data/chatRoomConstants.ts
+++ frontend/src/data/chatRoomConstants.ts
@@ -1,4 +1,4 @@
-export const WEB_SOCKET_URI: string = !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:5000/' : 'wss://chatbot-marc.herokuapp.com:5000/';
+export const WEB_SOCKET_URI: string = !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:5000/' : 'wss://0.0.0.0:5000/';
 export const WEB_SOCKET_CONFIG: any = {
   reconnectionAttempts: 5,
   reconnectionDelay: 5000,
