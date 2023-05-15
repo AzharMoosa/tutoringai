c3d80304f1d934ed22f7dd19fe1409cfa307f1a3
AzharMoosa <azhar.moosa8@gmail.com>
2023-05-15 17:54:44 +0100

    Implemented Rectangle Solver In Tutoring Engine

--- backend/common/tutoring_engine/shape_solver.py
+++ backend/common/tutoring_engine/shape_solver.py
@@ -1,4 +1,4 @@
-from backend.common.question_engine.graphics_question import TriangleQuestion, RectangleQuestion, CircleQuestion, Triangle
+from backend.common.question_engine.graphics_question import TriangleQuestion, RectangleQuestion, CircleQuestion, Triangle, Rectangle, Circle
 from typing import Union
 
 class ShapeQuestionSolver:
@@ -14,17 +14,31 @@ class ShapeQuestionSolver:
             s = (a + b + c) / 2.0
             area_squared = s * (s - a) * (s - b) * (s - c)
 
-            solution.append(f"To calculate the area of the triangle with side lengths {a} cm, {b} cm, {c} cm. We can use Heron's Formula.")
+            solution.append(f"To calculate the area of the triangle with side lengths {a} cm, {b} cm, {c} cm, we can use Heron's Formula.")
             solution.append(f"The area of the a triangle with side lengths a, b, c are A = sqrt(s * (s - a) * (s - b) * (s - c)), where s = (a + b + c) / 2")
             solution.append(f"In this case, the area is A = sqrt({s} * ({s} - {a}) * ({s} - {b}) * ({s} - {c})), where s = ({a} + {b} + {c}) / 2 = {s}")
             solution.append(f"A = sqrt({area_squared})")
             solution.append(f"A = {triangle.calculate_area()}")
+            solution.append(f"Therefore, the area of the triangle is {triangle.calculate_area():.3f}")
+
 
             return "\n".join(solution)
 
     @staticmethod
     def __solve_rectangle_question(question: RectangleQuestion):
-        pass
+        if (question.topic == "area"):
+            solution = []
+
+            rectangle = question.rectangle
+            width = rectangle.width
+            height = rectangle.height
+
+            solution.append(f"To calculate the area of the rectangle with with {width} cm and height {height} cm, we can multiply the width and height.")
+            solution.append(f"The area of the rectangle can be computed using: A = W * H")
+            solution.append(f"A = {width} * {height}")
+            solution.append(f"A = {rectangle.calculate_area()}")
+            solution.append(f"Therefore, the area of the rectangle is {rectangle.calculate_area():.3f}")
+            return "\n".join(solution)
 
     @staticmethod
     def __solve_circle_question(question: CircleQuestion):
@@ -43,12 +57,15 @@ class ShapeQuestionSolver:
             return ShapeQuestionSolver.__solve_circle_question(question)
         
 if __name__ == "__main__":
-    triangle = Triangle(1, 9, 9)
-    area = triangle.calculate_area()
-    triangle_question = TriangleQuestion("A triangle has side lengths 1cm, 9cm, 9cm. What is the area of the triangle?", "trigonometry", triangle, "area", area, "")
+    triangle = Triangle(1., 9., 9.)
+    triangle_area = triangle.calculate_area()
+    triangle_question = TriangleQuestion("A triangle has side lengths 1cm, 9cm, 9cm. What is the area of the triangle?", "trigonometry", triangle, "area", triangle_area, "")
 
     circle_area_question = "A circle has radius 8cm. What is the area of the circle?"
     circle_circumference_question = "A circle has radius 3cm. What is the circumference of the circle?"
-    rectangle_question = "A rectangle has width 3cm and height 3cm. What is the area of the rectangle?"
 
-    print(ShapeQuestionSolver.solve_question(triangle_question))
+    rectangle = Rectangle(3., 3.)
+    rectangle_area = rectangle.calculate_area()
+    rectangle_question = RectangleQuestion("A rectangle has width 3cm and height 3cm. What is the area of the rectangle?", "rectangle", rectangle, "area", rectangle_area, "")
+
+    print(ShapeQuestionSolver.solve_question(rectangle_question))
