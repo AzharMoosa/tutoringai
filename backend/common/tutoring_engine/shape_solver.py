15872cb899766868d16ab79a32c88b734840058f
AzharMoosa <azhar.moosa8@gmail.com>
2023-05-15 18:06:19 +0100

    Implemented Circle Solver In Tutoring Engine

--- backend/common/tutoring_engine/shape_solver.py
+++ backend/common/tutoring_engine/shape_solver.py
@@ -1,16 +1,16 @@
-from backend.common.question_engine.graphics_question import TriangleQuestion, RectangleQuestion, CircleQuestion, Triangle, Rectangle, Circle
+from backend.common.question_engine.graphics_question import TriangleQuestion, RectangleQuestion, CircleQuestion
 from typing import Union
 
 class ShapeQuestionSolver:
     @staticmethod
     def __solve_triangle_question(question: TriangleQuestion):
+        solution = []
+        triangle = question.triangle
+        a = triangle.a
+        b = triangle.b
+        c = triangle.c
+        
         if (question.topic == "area"):
-            solution = []
-
-            triangle = question.triangle
-            a = triangle.a
-            b = triangle.b
-            c = triangle.c
             s = (a + b + c) / 2.0
             area_squared = s * (s - a) * (s - b) * (s - c)
 
@@ -19,31 +19,48 @@ class ShapeQuestionSolver:
             solution.append(f"In this case, the area is A = sqrt({s} * ({s} - {a}) * ({s} - {b}) * ({s} - {c})), where s = ({a} + {b} + {c}) / 2 = {s}")
             solution.append(f"A = sqrt({area_squared})")
             solution.append(f"A = {triangle.calculate_area()}")
-            solution.append(f"Therefore, the area of the triangle is {triangle.calculate_area():.3f}")
+            solution.append(f"Therefore, the area of the triangle is {triangle.calculate_area():.3f} cm²")
 
 
-            return "\n".join(solution)
+        return "\n".join(solution)
 
     @staticmethod
     def __solve_rectangle_question(question: RectangleQuestion):
+        solution = []
+        rectangle = question.rectangle
+        width = rectangle.width
+        height = rectangle.height
+
         if (question.topic == "area"):
-            solution = []
-
-            rectangle = question.rectangle
-            width = rectangle.width
-            height = rectangle.height
-
             solution.append(f"To calculate the area of the rectangle with with {width} cm and height {height} cm, we can multiply the width and height.")
             solution.append(f"The area of the rectangle can be computed using: A = W * H")
             solution.append(f"A = {width} * {height}")
             solution.append(f"A = {rectangle.calculate_area()}")
-            solution.append(f"Therefore, the area of the rectangle is {rectangle.calculate_area():.3f}")
-            return "\n".join(solution)
+            solution.append(f"Therefore, the area of the rectangle is {rectangle.calculate_area():.3f} cm²")
+
+        return "\n".join(solution)
 
     @staticmethod
     def __solve_circle_question(question: CircleQuestion):
-        pass
+        solution = []
+        circle = question.circle
+        radius = circle.radius
 
+        if (question.topic == "area"):
+            solution.append(f"To calculate the area of a circle with radius {radius} cm, we can use the formula A = πr², where r is the radius of the circle.")
+            solution.append(f"A = πr²")
+            solution.append(f"A = π * {radius}²")
+            solution.append(f"A = {circle.calculate_area()}")
+            solution.append(f"Therefore, the area of the circle is {circle.calculate_area():.3f} cm²")
+        elif (question.topic == "circumference"):
+            solution.append(f"To calculate the circumference of a circle with radius {radius} cm, we can use the formula C = 2πr, where r is the radius of the circle.")
+            solution.append(f"C = 2πr")
+            solution.append(f"C = 2 * π * {radius}")
+            solution.append(f"C = {circle.calculate_circumference()}")
+            solution.append(f"Therefore, the circumference of the circle is {circle.calculate_circumference():.3f} cm")
+
+        return "\n".join(solution)
+         
     @staticmethod
     def solve_question(question: Union[TriangleQuestion, RectangleQuestion, CircleQuestion]):
         if isinstance(question, TriangleQuestion):
@@ -55,17 +72,3 @@ class ShapeQuestionSolver:
         elif isinstance(question, CircleQuestion):
             question.__class__ = CircleQuestion
             return ShapeQuestionSolver.__solve_circle_question(question)
-        
-if __name__ == "__main__":
-    triangle = Triangle(1., 9., 9.)
-    triangle_area = triangle.calculate_area()
-    triangle_question = TriangleQuestion("A triangle has side lengths 1cm, 9cm, 9cm. What is the area of the triangle?", "trigonometry", triangle, "area", triangle_area, "")
-
-    circle_area_question = "A circle has radius 8cm. What is the area of the circle?"
-    circle_circumference_question = "A circle has radius 3cm. What is the circumference of the circle?"
-
-    rectangle = Rectangle(3., 3.)
-    rectangle_area = rectangle.calculate_area()
-    rectangle_question = RectangleQuestion("A rectangle has width 3cm and height 3cm. What is the area of the rectangle?", "rectangle", rectangle, "area", rectangle_area, "")
-
-    print(ShapeQuestionSolver.solve_question(rectangle_question))
