ef316cfa1b03680cd2df1a100c82d57499aefb5a
AzharMoosa <azhar.moosa8@gmail.com>
2023-05-26 18:56:18 +0100

    Added wss:// prefix toSocket.io

--- frontend/src/data/chatRoomConstants.ts
+++ frontend/src/data/chatRoomConstants.ts
@@ -1,7 +1,7 @@
 export const WEB_SOCKET_URI: string =
   !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
     ? 'http://127.0.0.1:5000/'
-    : 'https://chatbot-marc.herokuapp.com/';
+    : 'wss://chatbot-marc.herokuapp.com/';
 export const WEB_SOCKET_CONFIG: any = {
   reconnectionAttempts: 5,
   reconnectionDelay: 5000,
