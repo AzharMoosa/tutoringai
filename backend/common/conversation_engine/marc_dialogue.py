511be7e9cda4af5b59b3401512aac18469d753dc
AzharMoosa <azhar.moosa8@gmail.com>
2023-05-27 12:04:43 +0100

    Improved Responses In Response Engine

---
+++ backend/common/conversation_engine/marc_dialogue.py
@@ -0,0 +1,28 @@
+import json
+import os
+import random
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+with open(f"{__location__}/../datasets/marc/marc-dialogue.json", 'r') as f:
+    marc_dialogue_json = f.read()
+
+marc_dialogue = json.loads(marc_dialogue_json)
+
+class MARCDialogue:
+    @staticmethod
+    def get_response(tag: str):
+        responses = next((response["responses"] for response in marc_dialogue if response["tag"] == tag), "As an AI Language model I am unable to answer this question.")
+        return random.choice(responses)
+    
+    @staticmethod
+    def get_uncertain_response():
+        return MARCDialogue.get_response("uncertain-responses")
+    
+    @staticmethod
+    def get_correct_response():
+        return MARCDialogue.get_response("correct-responses")
+    
+    @staticmethod
+    def get_incorrect_response():
+        return MARCDialogue.get_response("incorrect-responses")
--- backend/common/conversation_engine/response_engine.py
+++ backend/common/conversation_engine/response_engine.py
@@ -1,15 +1,20 @@
 import random
+from backend.common.conversation_engine.marc_dialogue import MARCDialogue
 from backend.common.question_engine.question_generation import QuestionGenerator
-
-uncertain_responses = ["Sorry, I did not understand the question!", 
-                       "I am unable to answer that question.", 
-                        "I didn't quite catch that. Please try again!"]
-correct_responses = ["Thats correct!"]
-incorrect_responses = ["Sorry that is wrong"]
 
 class ResponseEngine:
     @staticmethod
-    def __generate_next_question(question_index, question_list):
+    def __generate_next_question(question_index: int, question_list: list):
+        """
+        Returns the next question from the question list
+
+        Arguments:
+            question_index {int} The current question that user is on
+            question_list  {list} A list of questions to choose from
+
+        Returns:
+            {dict} - Updated Question State
+        """
         return {
             "currentQuestion": question_list[question_index], 
             "questionList": question_list,
@@ -18,7 +23,7 @@ class ResponseEngine:
 
     @staticmethod
     def get_response(intents, tag: str):
-        return next((random.choice(intent["responses"]) for intent in intents if intent["tag"] == tag), random.choice(uncertain_responses))
+        return next((random.choice(intent["responses"]) for intent in intents if intent["tag"] == tag), MARCDialogue.get_uncertain_response())
 
     @staticmethod
     def generate_message(message_content: str, is_answering: bool, state: dict={}):
@@ -26,23 +31,23 @@ class ResponseEngine:
     
     @staticmethod
     def generate_uncertain_response() -> dict:
-        message = random.choice(uncertain_responses)
+        message = MARCDialogue.get_uncertain_response()
         return ResponseEngine.generate_message(message, is_answering=False)
 
     @staticmethod
     def generate_incorrect_response(state):
-        message = random.choice(incorrect_responses)
+        message = MARCDialogue.get_incorrect_response()
         return ResponseEngine.generate_message(message, is_answering=True, state=state)
     
     @staticmethod
     def generate_finish_answering_response():
-        message = f"{random.choice(correct_responses)}. That's all for now."
+        message = f"{MARCDialogue.get_correct_response()}. That's all for now."
         return ResponseEngine.generate_message(message, False)
     
     @staticmethod
     def generate_next_question_response(state, question_index):
         new_state = ResponseEngine.__generate_next_question(question_index, state["questionList"])
-        message = f"{random.choice(correct_responses)}. Let's try another question. " + new_state["currentQuestion"]["question"]
+        message = f"{MARCDialogue.get_correct_response()}. Let's try another question. " + new_state["currentQuestion"]["question"]
         return ResponseEngine.generate_message(message, True, new_state)
     
     @staticmethod
